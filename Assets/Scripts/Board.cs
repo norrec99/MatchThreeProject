039ecfc40a17c983a5de0af47a27149214cbbce6
cagataysoyer <cagatay@stardust.gs>
2023-08-01 19:10:26 +0300

    setup camera

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -6,18 +6,26 @@ public class Board : MonoBehaviour
 {
     [SerializeField] private int width;
     [SerializeField] private int height;
+    [SerializeField] private int borderSize;
     [SerializeField] private GameObject tilePrefab;
 
     private Tile[,] allTiles;
+    private Camera mainCamera;
+
+    private void Awake() 
+    {
+        mainCamera = Camera.main;    
+    }
 
     // Start is called before the first frame update
     void Start()
     {
         allTiles = new Tile[width,height];
-        SetupBoard();
+        SetupTiles();
+        SetupCamera();
     }
 
-    private void SetupBoard()
+    private void SetupTiles()
     {
         for (int i = 0; i < width; i++)
         {
@@ -25,11 +33,22 @@ public class Board : MonoBehaviour
             {
                 GameObject tile = Instantiate(tilePrefab, new Vector3(i, j, 0), Quaternion.identity);
                 tile.name = $"Tile {i}, {j}";
+                tile.transform.SetParent(transform);
 
                 allTiles[i,j] = tile.GetComponent<Tile>();
 
             }
         }
+    }
+
+    private void SetupCamera()
+    {
+        mainCamera.transform.position = new Vector3((float)(width - 1) / 2f, (float)(height - 1) / 2f, -10f);
+
+        float aspectRatio = (float)Screen.width / (float)Screen.height;
+        float verticalSize = (float)height / 2f + (float) borderSize;
+        float horizontalSize = ((float)width / 2f + (float) borderSize) + aspectRatio;
+        mainCamera.orthographicSize = (verticalSize > horizontalSize) ? verticalSize : horizontalSize;
     }
 
 
